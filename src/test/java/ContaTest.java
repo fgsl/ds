e191436fd03f3a12418979c7df79350651c95d54
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2026-02-26 22:02:10 -0300

    Criação de estrutura de testes

---
+++ src/test/java/ContaTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ContaTest {
+    @Test
+    void verificarSaldoDaConta() {
+        Conta conta = new Conta();
+        conta.saldo = 4500.60;
+        assertEquals(4500.60,conta.saldo);
+    }
+}
