c450e30ac5df0d530d2fa5388fe6e103bcae10f5
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2026-04-02 21:16:39 -0300

    Add files via upload

---
+++ TabuleiroPolarizado.java
@@ -0,0 +1,10 @@
+public class TabuleiroPolarizado {
+    public static void main(String[] args) {
+
+        System.out.println("O O O");
+        System.out.println("O X X");
+        System.out.println("X X X");
+
+        System.out.println("Criado por Guilherme Caparroz");
+    }
+}
