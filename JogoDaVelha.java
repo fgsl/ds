827be57e88645b8bc9750fd96499efc85d9da408
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2026-04-02 22:26:57 -0300

    Add files via upload

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -4,9 +4,12 @@ class JogoDaVelha
 {
     public static void main(String args[])
     {
-        Jogador jogador1 = new Jogador('X');
-        Jogador jogador2 = new Jogador('O');
-
+        //Jogador jogador1 = new Jogador('X');
+        //Jogador jogador2 = new Jogador('O');
+        Jogador jogador1, jogador2;
+        jogador1 = new Jogador('X');
+        jogador2 = new Jogador('O');
+        
         Tabuleiro tabuleiro = new Tabuleiro();
 
         int linha,coluna;
@@ -15,17 +18,28 @@ class JogoDaVelha
 
 
         // Enquanto não acabou o jogo
+        int partida = 1;
         while(!tabuleiro.acabouOJogo())
         {
-            linha = random.nextInt(0,3);
-            coluna = random.nextInt(0,3);
-    
-            tabuleiro.jogar(jogador1.getSimbolo(),linha,coluna);
+            System.out.println("Partida " + partida);
 
-            linha = random.nextInt(0,3);
-            coluna = random.nextInt(0,3);
+            do {
+                linha = random.nextInt(0,3);
+                coluna = random.nextInt(0,3);
+            }    
+            while (!tabuleiro.jogar(jogador1.getSimbolo(),linha,coluna));
 
-            tabuleiro.jogar(jogador2.getSimbolo(),linha,coluna);
+            if (tabuleiro.acabouOJogo())
+            {
+                break;
+            }
+
+            do {
+                linha = random.nextInt(0,3);
+                coluna = random.nextInt(0,3);
+            }    
+            while(!tabuleiro.jogar(jogador2.getSimbolo(),linha,coluna));
+            partida++;
         }
     }
 }
